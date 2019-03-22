65d59eb95a6cd779d5eefc5a33b31054e16c9f96
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 11:26:49 +0530

    Version Checking

---
+++ Basic uses/Checking the Version of Opencv.py
@@ -0,0 +1,12 @@
+#importing opencv2 module
+import cv2
+
+#defining main function
+
+def main():
+    
+    #printing version of opencv2 just checking
+    print(cv2.__version__)
+    
+if __name__ == "__main__":
+    main()
