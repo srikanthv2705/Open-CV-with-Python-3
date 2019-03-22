bd9e96045e7e81c9c4c9b1cbee40161704a8b09f
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 18:35:30 +0530

    Add files via upload

---
+++ Basic uses/35.Object Tracking by color Blue.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+def main():
+    
+    vid_cap = cv2.VideoCapture(0)    
+  
+    if vid_cap.isOpened():
+        ret, frame = vid_cap.read()
+        
+    else:
+        ret = False
+        
+    while ret:
+        ret, frame = vid_cap.read()
+        
+        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        
+        #Blue Color Tracking
+        low = np.array([100,50,50])
+        high = np.array([140,255,255])
+        
+        image_mask =cv2.inRange(hsv,low,high)
+              
+        cv2.imshow("Image Mask",image_mask)
+        print(image_mask)
+        cv2.imshow("Web cam preview", frame)
+        
+        if cv2.waitKey(1) == 27: #it exits when we press esc key
+            break
+    cv2.destroyAllWindows()
+    vid_cap.release()
+    
+if __name__ == "__main__":
+    main()
+            
