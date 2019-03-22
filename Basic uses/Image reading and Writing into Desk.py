1dd043944c5894b16287f38be7642285391bedc7
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 11:52:50 +0530

    Imaging reading and writing

---
+++ Basic uses/Image reading and Writing into Desk.py
@@ -0,0 +1,17 @@
+import cv2
+
+def main():
+    
+    source = "C:\\Users\\Srikanth\\Desktop\\Dataset\\New folder\\4.2.04.tiff"
+    img = cv2.imread(source,0) # 0 indiactes the gray scale image in converts color image to gray scale
+    
+    output = "C:\\Users\\Srikanth\\Desktop\\Dataset\\output\\4.2.04.jpg" #Changing into JPG format
+        
+    cv2.imshow('Lena',img)
+    cv2.imwrite(output,img )
+    cv2.waitKey(0)
+    cv2.destroyAllWindows('Lena')
+    
+if __name__ == "__main__":
+    main()
+    
