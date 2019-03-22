1ed5fee26665b92146a30e079a7602dc730b710e
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 12:02:41 +0530

    Image Reading and writing into a folder

--- Basic uses/Image reading and Writing into Desk.py
+++ Basic uses/Image reading and Writing into Desk.py
@@ -5,13 +5,14 @@ def main():
     source = "C:\\Users\\Srikanth\\Desktop\\Dataset\\New folder\\4.2.04.tiff"
     img = cv2.imread(source,0) # 0 indiactes the gray scale image in converts color image to gray scale
     
-    output = "C:\\Users\\Srikanth\\Desktop\\Dataset\\output\\4.2.04.jpg" #Changing into JPG format
+    output = "C:\\Users\\Srikanth\\Desktop\\Dataset\\New folder\\output\\4.2.04.jpg" #Changing into JPG format
         
-    cv2.imshow('Lena',img)
-    cv2.imwrite(output,img )
+    cv2.imshow('Lena', img)
+    cv2.imwrite(output, img)
     cv2.waitKey(0)
-    cv2.destroyAllWindows('Lena')
+    cv2.destroyWindow('Lena')
+    
+    
     
 if __name__ == "__main__":
     main()
-    
