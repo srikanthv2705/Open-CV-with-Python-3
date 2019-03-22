e8c6f5c0799835f8f9cc4c8a4079ab3de3916e17
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 12:42:21 +0530

    Geometric Shapes of Line

---
+++ Basic uses/Geometric Shapes.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def main():
+    
+    img = np.zeros((512,512,3),np.uint8) # it indicates color image
+    
+    cv2.line(img,(0,100),(100,0),(255,0,0),2) #last tuple indicates color 1st one is for Blue, last one is for thickness of line
+    
+    cv2.imshow('Lena', img)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Lena')
+    
+    
+    
+if __name__ == "__main__":
+    main()
