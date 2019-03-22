404d0d9961764f1cf2f11ec6775246ffca37309f
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 15:32:31 +0530

    Mouse Events and Drawing

--- Basic uses/17. Interactive Drawing and Mouse Events1.py
+++ Basic uses/17. Interactive Drawing and Mouse Events1.py
@@ -6,30 +6,50 @@ window_Name = "Painting"
 img = np.zeros((512,512,3), np.uint8)
 cv2.namedWindow(window_Name)
 
-#Mouse callback function here we are using Left button Double click
-def draw_circle(event,x, y, flags, param ):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img,(x,y),40,(0,0,255),-1)
-#Mouse callback function here we are using Middle button Down
-    if event == cv2.EVENT_MBUTTONDOWN:
-        cv2.circle(img,(x,y),10,(0,255,255),-1)
+#if mouse is pressed it is True
+paint = False
 
-#Mouse callback function here we are using Left button Down
+#if True, Draw Rectangle. Press 'm' to toggle to curve
+mode = True
+(ix, iy) =(-1,-1)
+
+#Mouse callback function 
+def draw_event(event,x, y, flags, param ):
+    global ix, iy, paint, mode
+    
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img,(x,y),30,(0,255,0),-1)
+        paint = True
+        (ix, iy) = x , y
+    
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if paint == True:
+            if mode == True:
+                cv2.rectangle(img,(ix,iy),(x,y),(0,0,255),-1)
+            else:
+                cv2.circle(img,(x,y),10,(0,255,0),-1)
 
+    elif event == cv2.EVENT_LBUTTONUP:
+        if paint == False:
+            if mode == True:
+                cv2.rectangle(img,(ix,iy),(x,y),(0,0,255),-1)
+            else:
+                cv2.circle(img,(x,y),10,(0,255,0),-1)                
 
 #bind the call back function to window
-cv2.setMouseCallback(window_Name, draw_circle)
+cv2.setMouseCallback(window_Name, draw_event)
 
 def main():
+    global mode
     
-   while True:
-       cv2.imshow(window_Name,img)
-       if cv2.waitKey(20) == 27:
-           break
-       
-   cv2.destroyAllWindows() 
-       
+    while True:
+        cv2.imshow(window_Name,img)
+        
+        k = cv2.waitKey(1)
+        if k == ord('m') or k == ord("M"):
+            mode = not mode
+        elif k == 27:
+            break
+    cv2.destroyAllWindows()
+        
 if __name__ == "__main__":
     main()
