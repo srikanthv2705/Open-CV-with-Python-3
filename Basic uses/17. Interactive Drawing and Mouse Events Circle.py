a8ed11f078a2b77cc5975ddce4889ae71338eaf4
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 15:08:36 +0530

    Mouse events to draw a circle

---
+++ Basic uses/17. Interactive Drawing and Mouse Events Circle.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+#Create a black image window
+window_Name = "Painting"
+img = np.zeros((512,512,3), np.uint8)
+cv2.namedWindow(window_Name)
+
+#Mouse callback function here we are using Left button Double click
+def draw_circle(event,x, y, flags, param ):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img,(x,y),40,(0,0,255),-1)
+
+#bind the call back function to window
+cv2.setMouseCallback(window_Name, draw_circle)
+
+def main():
+    
+   while True:
+       cv2.imshow(window_Name,img)
+       if cv2.waitKey(20) == 27:
+           break
+       
+   cv2.destroyAllWindows() 
+       
+if __name__ == "__main__":
+    main()
