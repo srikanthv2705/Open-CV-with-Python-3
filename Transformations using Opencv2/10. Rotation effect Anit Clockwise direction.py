7648537a1661196289a69b1dd54138d447db8188
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 23:51:07 +0530

    Add files via upload

---
+++ Transformations using Opencv2/10. Rotation effect Anit Clockwise direction.py
@@ -0,0 +1,38 @@
+import cv2
+import time
+
+def main():
+    
+    source = "C:\\Users\\Srikanth\\Desktop\\Python\\Opencv\\Dataset\\"
+    
+    imgpath1 = source + "4.2.01.tiff"
+    
+    img = cv2.imread(imgpath1,1)
+    
+    rows, columns, channels = img.shape
+    
+    angle = 0
+    
+    while True:
+        
+        if angle == 360:
+            angle = 0 #Rotating Anti Clockwise Direction
+        
+        R = cv2.getRotationMatrix2D((columns/2, rows/2), angle, 0.7)
+        
+        print(R)
+    
+        output = cv2.warpAffine(img, R, (columns, rows))
+    
+    
+        cv2.imshow('Rotating Image', output)
+        angle += 1
+        time.sleep(0.01)
+        
+        if cv2.waitKey(1) == 27:
+            break
+        
+    cv2.destroyWindow('Rotating Image')
+
+if __name__ == "__main__":
+    main()
