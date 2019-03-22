ae246e2f57f4f1f98852b69b08c94d9aa576458e
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 18:00:17 +0530

    Add files via upload

--- Basic uses/34. Web Cam Video Recording to a file1.py
+++ Basic uses/34. Web Cam Video Recording to a file1.py
@@ -1,39 +1,25 @@
 import cv2
 
 def main():
-    windowName = "Webcam Live Video"
+    windowName = "Video Playback"
     cv2.namedWindow(windowName)
-    vid_cap = cv2.VideoCapture(0)
-    
     filename = "C:\\Users\\Srikanth\\Desktop\\Dataset\\New folder\\output\\webcam_recording.avi"
-    codec = cv2.VideoWriter_fourcc('X','V','I','D') #ither popular encoding formats are WMV2,WMV1,MJPG,DIVX,XVID,H264 these are four character codes
+    vid_cap = cv2.VideoCapture(filename)
     
-    framerate = 30   #smooth frame rate
-    resolution = (640,480) #lower resolution the frame is smoother
+    ret = True
     
-    videofile_output = cv2.VideoWriter(filename,codec,framerate,resolution)
-    
-    
-    if vid_cap.isOpened():
-        ret, frame = vid_cap.read()
-    else:
-        ret = False
-
     while ret:
-    
-        ret, frame = vid_cap.read()
-        
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        
-        videofile_output.write(frame)
-        
-        cv2.imshow(windowName, frame)
-    
-        if cv2.waitKey(1) == 27:
+        if vid_cap.isOpened():  
+            ret, frame = vid_cap.read()
+
+        if ret:
+            cv2.imshow(windowName,frame)
+            if cv2.waitKey(33) == 27: #fps rate should match with Video play back rate
+                break
+        else:
             break
 
     cv2.destroyAllWindows()    
-    videofile_output.release()
     vid_cap.release()
 
 if __name__ == "__main__":
