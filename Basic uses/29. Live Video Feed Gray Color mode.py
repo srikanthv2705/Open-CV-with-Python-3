e8d48dca6c2a950aa4278ff2375ebddb32b3a0c4
voleti143 <39414475+voleti143@users.noreply.github.com>
2019-03-22 17:11:40 +0530

    Add files via upload

---
+++ Basic uses/29. Live Video Feed Gray Color mode.py
@@ -0,0 +1,28 @@
+import cv2
+
+def main():
+    windowName = "Webcam Live Video"
+    cv2.namedWindow(windowName)
+    vid_cap = cv2.VideoCapture(0)
+    
+    if vid_cap.isOpened():
+        ret, frame = vid_cap.read()
+    else:
+        ret = False
+
+    while ret:
+    
+        ret, frame = vid_cap.read()
+        
+        output = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        
+        cv2.imshow(windowName, output)
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyAllWindows()    
+
+    vid_cap.release()
+
+if __name__ == "__main__":
+    main()
